eab0f20cb7b7c7ecdc5e8b7673895a2868438732
Tiego <tiegoparotivo@gmail.com>
2016-09-01 12:00:46 -0300

    Retirando código irrelevante

--- Annotations/src/Aplicacao.java
+++ Annotations/src/Aplicacao.java
@@ -3,8 +3,6 @@
 public class Aplicacao {
 
 	public static void main(String[] args) throws Exception {
-		
-		ObjectCreator.create(Teste.class);		
-		
+		ObjectCreator.create(Teste.class);			
 	}
 }
--- Annotations/src/ObjectCreator.java
+++ Annotations/src/ObjectCreator.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 /**
@@ -14,47 +15,55 @@ public class ObjectCreator {
 	 * Método que instancia e chama os métodos automaticamente após a construção
 	 * do objeto, caso seja necessário.
 	 * 
+	 * @param <T>
+	 *            Classe que terá seu objeto instanciado e seus métodos
+	 *            chamados.
+	 * 
 	 * @param clazz
 	 *            Classe utilizada na instanciação do objeto.
 	 * @return Objeto criado e com os métodos invocados. Se o objeto não puder
 	 *         ser criado, o retorno é null.
+	 * @throws InvocationTargetException
+	 *             Lançado quando há algum problema na invocação do método por
+	 *             meio de Reflexão.
+	 * @throws IllegalArgumentException
+	 *             Lançado quando um método passa um parâmetro ilegal ou
+	 *             inapropriado.
+	 * @throws IllegalAccessException
+	 *             Lançado quando o método não tem acesso a classe que será
+	 *             instanciada
+	 * @throws InstantiationException
+	 *             Lançada quando a classe não puder ser instanciada.
 	 */
-	public static <T> T create(Class<T> clazz) {
+	public static <T> void create(Class<T> clazz)
+			throws IllegalAccessException, IllegalArgumentException,
+			InvocationTargetException, InstantiationException {
 
-		try {
-			/* Cria uma instância da classe. */
-			T obj = (T) clazz.newInstance();
+		/* Cria uma instância da classe. */
+		T obj = (T) clazz.newInstance();
 
-			/* Obtém um array com os métodos da classe. */
-			Method[] methods = clazz.getMethods();
-			
-			int random = 1 + (int) (Math.random() * 3);
-			
-			/* Itera sobre cada método da classe. */
-			for (Method method : methods) {
+		/* Obtém um array com os métodos da classe. */
+		Method[] methods = clazz.getMethods();
 
-				/*
-				 * Obtém a anotação "exec" do método. Se não tiver a anotação,
-				 * retorna null.
-				 */
-				Executar exec = method.getAnnotation(Executar.class);
+		int random = 1 + (int) (Math.random() * 3);
 
-				/*
-				 * Invoca o método caso haja anotação e o elemento seja igual ao número randômico sorteado.
-				 */
-				if (exec != null && exec.numRandom() == random) {
-					method.invoke(obj);
-					
-				}
-				
+		/* Itera sobre cada método da classe. */
+		for (Method method : methods) {
+
+			/*
+			 * Obtém a anotação "exec" do método. Se não tiver a anotação,
+			 * retorna null.
+			 */
+			Executar exec = method.getAnnotation(Executar.class);
+
+			/*
+			 * Invoca o método caso haja anotação e o elemento seja igual ao
+			 * número randômico sorteado.
+			 */
+			if (exec != null && exec.numRandom() == random) {
+				method.invoke(obj);
+
 			}
-			
-			/* Retorna o objeto criado. */
-			return obj;
-		} catch (Exception e) {
-			
-			/* Se houver algum problema, retorna null. */ 
-			return null;
 		}
 	}
 }
